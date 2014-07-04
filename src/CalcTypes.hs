b32e815936c41780e4974aaac14356723c100dfa
Richard Appleby <appleby_richard@msn.com>
2014-07-04 19:25:23 +0100

    Simplified calculator code, removed variables for now

--- src/CalcTypes.hs
+++ src/CalcTypes.hs
@@ -1,9 +1,7 @@
 module CalcTypes where
 
-data Expr = Lit Integer | Var Var | Op Ops Expr Expr	deriving (Eq,Show)
+data Expr = Lit Integer | Op Ops Expr Expr deriving (Eq,Show)
 
-data Ops  = Add | Sub | Mul | Div | Mod	 		deriving (Eq,Show)
+data Ops  = Add | Sub | Mul | Div | Mod deriving (Eq,Show)	
 
-type Var  = Char				
-
-data Command = Eval Expr | Assign Var Expr | Null	deriving (Eq,Show)
+data Command = Eval Expr | Null	deriving (Eq,Show)
